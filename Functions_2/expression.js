18be4995b8b25794ba14b10c65cec0811b4f94c5
sonerzorlu <sonerzorlu@gmail.com>
2023-05-30 17:18:56 +0300

    practice

--- Functions_2/expression.js
+++ Functions_2/expression.js
@@ -80,71 +80,45 @@ console.log(oddOrEven(8));
 //* ORNEK4:
 //*****************************************/
 
-// const add = function (n1, n2) {
-//   return n1 + n2;
-// };
+const add = function (n1, n2) {
+  return n1 + n2;
+};
 
-// const sub = function (n1, n2) {
-//   return n1 - n2;
-// };
+const sub = function (n1, n2) {
+  return n1 - n2;
+};
 
-// const div = function (n1, n2) {
-//   return n1 / n2;
-// };
+const div = function (n1, n2) {
+  return n1 / n2;
+};
 
-// const mul = function (n1, n2) {
-//   return n1 * n2;
-// };
+const mul = function (n1, n2) {
+  return n1 * n2;
+};
 
-// const compute = function (n1, n2, op) {
-//   let result = 0;
-//   switch (op) {
-//     case "+":
-//       result = add(n1, n2);
-//       break;
-//     case "-":
-//       result = sub(n1, n2);
-//       break;
-//     case "*":
-//       result = mul(n1, n2);
-//       break;
-//     case "/":
-//       result = div(n1, n2);
+const compute = function (n1, n2, op) {
+  let result = 0;
+  switch (op) {
+    case "+":
+      result = add(n1, n2);
+      break;
+    case "-":
+      result = sub(n1, n2);
+      break;
+    case "*":
+      result = mul(n1, n2);
+      break;
+    case "/":
+      result = div(n1, n2);
 
-//       break;
+      break;
 
-//     default:
-//       break;
-//   }
-//   return result;
-// };
-// console.log(compute(4, 3, "/"));
-// console.log(compute(2, 4, "*"));
-// console.log(compute(3, 1, "+"));
-// console.log(compute(4, 3, "-"));
-
-// const toCelcius = function (f) {
-
-//     return (5/9) * (f-32)
-    
-//     }
-    
-//     console.log(toCelcius(100))
-
-//* ORNEK4: 
-//*****************************************/
-// // ! Ucgenin alani
-
-// const alan = function (a,b){
-//     return (a*b)/2
-// }
-// console.log(`ucgenin alani ${alan(8,16)} dur`);
-
-//* ORNEK5: 
-//*****************************************/
-
-const yil = function(year){
-    return year%400===0 ? "artik yildir" : " degildir"
-}
-
-console.log(yil(2023));
+    default:
+      break;
+  }
+  return result;
+};
+console.log(compute(4, 3, "/"));
+console.log(compute(2, 4, "*"));
+console.log(compute(3, 1, "+"));
+console.log(compute(4, 3, "-"));
