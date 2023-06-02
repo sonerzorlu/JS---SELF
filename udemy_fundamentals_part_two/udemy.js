f157f44f22a1ccd9c5825a9cd338e809c5bffd39
sonerzorlu <sonerzorlu@gmail.com>
2023-06-02 10:26:43 +0300

    array practice

--- udemy_fundamentals_part_two/udemy.js
+++ udemy_fundamentals_part_two/udemy.js
@@ -231,17 +231,19 @@
 //     console.log(soner[i]);
 // }
 
-const data1= [17,21,23];
+// const data1= [17,21,23];
 
-console.log(`...${data1[0]}°C ...${data1[1]}°C ...${data1[2]}°C ...`);
+// console.log(`...${data1[0]}°C ...${data1[1]}°C ...${data1[2]}°C ...`);
 
-const printForecast = function(arr){
-let str =''
-    for(i=0;i<arr.length;i++){
-        str = str + `${arr[i]}°C in ${i + 1}days... `;
+// const printForecast = function(arr){
+// let str =''
+//     for(i=0;i<arr.length;i++){
+//         str = str + `${arr[i]}°C in ${i + 1}days... `;
 
-    } 
-    console.log(str);
-}
-printForecast(data1)
+//     } 
+//     console.log(str);
+// }
+// printForecast(data1)
+
+
    
